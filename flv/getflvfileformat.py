58e5e88b9a4724fcf4eb4596b2f215121c6e0f7a
liuzhenhua03 <liuzhenhua03@baidu.com>
2015-06-02 22:10:59 +0800

    init

---
+++ flv/getflvfileformat.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: flonly
+# @Date:   2015-06-02 21:07:34
+# @Last Modified by:   flonly
+# @Last Modified time: 2015-06-02 21:54:29
+
+from BitRead import BitRead
+
+class FLV
+    self.file='o.txt' 
+
+    def __init__():
+        self.ofile='output/o.txt'
+        self.ifile='res/i.flv'
+        self.ofd = open(self.ofile,'w')
+        self.ifd = open(self.ifile,'r')
+        self.bitReader = new BitRead(self.ifd)
+        return True
+
+    def getHeader(fd):
+        
+
+
+
+
+    def readBits(fd,size):
+
+
+    def start():
+
+
+
+if __name__ == '__main__':
+    main()
